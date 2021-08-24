9a53895bed587c7698880e3deecad003f30153c0
Andreas-V <54800767+Andreas-V-TE18D@users.noreply.github.com>
2021-08-24 15:38:41 +0200

    övningar 1 till 3

--- lib/intro.ex
+++ lib/intro.ex
@@ -2,4 +2,15 @@ defmodule Intro do
   def hello do
     :world
   end
+  def smallest(n1, n2) when n1< n2 , do: n1
+  def smallest(_n1, n2), do: n2
+
+  def smallest(n1, n2, n3, n4) do
+    x=smallest(n1, n2)
+    y=smallest(n3, n4)
+    smallest(x, y)
+
+  end
+
+
 end
--- test/intro_test.exs
+++ test/intro_test.exs
@@ -1,7 +1,21 @@
 defmodule IntroTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert Intro.hello() == :world
+  test "smallest of two when smallest is first" do
+    assert Intro.smallest(1,2) == 1
+
   end
+  test "smallest of two when smallest is last" do
+    assert Intro.smallest(3,2) == 2
+
+
+end
+test "smallest of two of equal size" do
+  assert Intro.smallest(1337,1337) == 1337
+end
+
+test"smallest of four" do
+  assert Intro.smallest(1873,9143,8347,3477)
+end
+test
 end
